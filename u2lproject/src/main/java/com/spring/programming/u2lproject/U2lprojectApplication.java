c1a41899d045fd34c2a0235fcf37167ec50401cc
Garamkim <kkll445@sk.com>
2023-06-02 13:58:32 +0900

    FIRST Commit

---
+++ u2lproject/src/main/java/com/spring/programming/u2lproject/U2lprojectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.programming.u2lproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2lprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2lprojectApplication.class, args);
+	}
+
+}
---
+++ u2lproject/src/test/java/com/spring/programming/u2lproject/U2lprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.programming.u2lproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2lprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
